941486358b51f4554bee41ea4e913035aba171b8
okawaffles <okawaffles@gmail.com>
2025-01-07 12:29:45 -0600

    market updating

--- modules/okash/stock.ts
+++ modules/okash/stock.ts
@@ -26,7 +26,7 @@ export enum Stocks {
 
 // --
 
-let DB_PATH;
+let DB_PATH: string;
 let MARKET: StockMarket;
 
 // --
@@ -47,21 +47,21 @@ export function SetupStocks(dirname: string) {
     MARKET = {
         catgirl: {
             price: 500,
-            price_history: [500],
+            price_history: [500,500],
             holders: [],
             name: 'Catgirl',
             id: 'CTGL'
         },
         doggirl: {
             price: 300,
-            price_history: [300],
+            price_history: [300,300],
             holders: [],
             name: 'Doggirl',
             id: 'DOGY'
         },
         foxgirl: {
             price: 150,
-            price_history: [150],
+            price_history: [150,150],
             holders: [],
             name: 'Foxgirl',
             id: 'FXGL'
@@ -80,3 +80,40 @@ export function GetSharePrice(stock: Stocks): number {
     return MARKET[stock].price;
 }
 
+/**
+ * Updates the prices of the stocks slightly.
+ * It will take note of the current trend and have a 1/8 chance of flipping the trend.
+ * If the trend is no change (e.g. 150,150) then it will assume a positive trend.
+ * This is so that the stock prices hopefully don't go insane rollercoaster 24/7.
+ */
+export function UpdateMarkets() {
+    enum Trend {
+        POSITIVE = 1,
+        NEGATIVE = -1
+    };
+    let trend = Trend.POSITIVE;
+
+    // catgirl market:
+    if (MARKET.catgirl.price_history[-1] < MARKET.catgirl.price_history[-2]) trend = Trend.NEGATIVE;
+    if (Math.random() < 1/8) trend = trend * -1; // flips polarity on the 1/8 chance, essentially swapping it
+    let change = trend * Math.floor(Math.random() * 3);
+    MARKET.catgirl.price += change;
+    MARKET.catgirl.price_history.push(MARKET.catgirl.price);
+
+    // doggirl market:
+    if (MARKET.doggirl.price_history[-1] < MARKET.doggirl.price_history[-2]) trend = Trend.NEGATIVE;
+    if (Math.random() < 1/8) trend = trend * -1; // flips polarity on the 1/8 chance, essentially swapping it
+    change = trend * Math.floor(Math.random() * 3);
+    MARKET.doggirl.price += change;
+    MARKET.doggirl.price_history.push(MARKET.doggirl.price);
+    
+    // foxgirl market:
+    if (MARKET.foxgirl.price_history[-1] < MARKET.foxgirl.price_history[-2]) trend = Trend.NEGATIVE;
+    if (Math.random() < 1/8) trend = trend * -1; // flips polarity on the 1/8 chance, essentially swapping it
+    change = trend * Math.floor(Math.random() * 3);
+    MARKET.foxgirl.price += change;
+    MARKET.foxgirl.price_history.push(MARKET.foxgirl.price);
+
+    // write changes
+    writeFileSync(DB_PATH, JSON.stringify(MARKET), 'utf-8');
+}
