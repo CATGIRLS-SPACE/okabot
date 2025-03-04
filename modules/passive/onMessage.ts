1851a25f3eed3181234360b6a111a10446e734f3
Lilia Aurora <oka@waffle.moe>
2025-03-03 19:55:45 -0600

    increase drop rates

--- modules/passive/onMessage.ts
+++ modules/passive/onMessage.ts
@@ -17,15 +17,15 @@ export async function DoRandomDrops(message: Message) {
     const time = Math.round(new Date().getTime() / 1000);
     const boost_active = (BoostsActive.has(message.author.id) && BoostsActive.get(message.author.id)! > time);
 
-    // usually 1 in 500 chance, boost is 1 in 300
-    const small_roll = boost_active?Math.floor(Math.random() * 300):Math.floor(Math.random() * 500);
-    // usually 1 in 2500, boost is 1 in 1500
-    const large_roll = boost_active?Math.floor(Math.random() * 1500):Math.floor(Math.random() * 2500);
-    // usually 1 in 5000, boost is 1 in 1700 <-- low so that incentive is high
-    const ex_roll = boost_active?Math.floor(Math.random() * 1700):Math.floor(Math.random() * 5000);
+    // usually 1 in 500 chance, boost is 1 in 300 <-- these are old, ignore them
+    const small_roll = boost_active?Math.floor(Math.random() * 150):Math.floor(Math.random() * 250);
+    // usually 1 in 2500, boost is 1 in 1500 <-- these are old, ignore them
+    const large_roll = boost_active?Math.floor(Math.random() * 400):Math.floor(Math.random() * 800);
+    // usually 1 in 5000, boost is 1 in 1700 <-- low so that incentive is high <-- these are old, ignore them
+    const ex_roll = boost_active?Math.floor(Math.random() * 700):Math.floor(Math.random() * 2000);
 
     switch (small_roll) {
-        case 250:
+        case 127:
             // you cannot earn okash drops with a boost on since they use the same roll variable
             if (boost_active) break;
             L.info('trigger small reward');
@@ -37,7 +37,7 @@ export async function DoRandomDrops(message: Message) {
             AddToWallet(message.author.id, found_amount);
             break;
 
-        case 297:
+        case 97:
             L.info('trigger common lootbox');
             const cl_reply = await message.reply(':anger: Ow..!?');
             await Sleep(3000);
@@ -51,7 +51,7 @@ export async function DoRandomDrops(message: Message) {
     }
 
     switch (large_roll) {
-        case 1561:
+        case 382:
             if (boost_active) break;
             L.info('trigger large reward');
             const found_amount = Math.ceil(Math.random() * 5000) + 5000; // 5000-10000
@@ -75,7 +75,7 @@ export async function DoRandomDrops(message: Message) {
             break;
     }
 
-    if (ex_roll == 1234) {
+    if (ex_roll == 679) { // im like yea she's fine, wonder when she'll be mine
         L.info('trigger ex lootbox');
         const cl_reply = await message.reply(':anger: Ow..!?');
         await Sleep(3000);
