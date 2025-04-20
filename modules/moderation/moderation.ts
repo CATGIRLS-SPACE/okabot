c380e68d7f0d1137b5c91d7f6ab1a43a5fa9ec99
Lilia Aurora <okawaffles@gmail.com>
2025-04-20 18:42:15 -0500

    write like 3 lines because im lazy again

---
+++ modules/moderation/moderation.ts
@@ -0,0 +1,13 @@
+import {Logger} from "okayulogger";
+import {Message} from "discord.js";
+
+
+const L = new Logger('moderation');
+
+
+/**
+ * Checks for moderation shorthands such as o.ban or o.kick or o.mute
+ */
+export async function CheckModerationShorthands(message: Message) {
+
+}
