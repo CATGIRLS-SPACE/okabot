b44aa03dd1457715aa7c19106841f8443de3da33
Millie <oka@waffle.moe>
2025-06-19 19:52:01 -0500

    bump to 4.10.0

---
+++ modules/textbased/patchnotes/versions/4.10.0.ts
@@ -0,0 +1,35 @@
+import {ContainerBuilder, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder} from "discord.js";
+
+
+export const PATCHNOTES_COMPONENT = new ContainerBuilder()
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('# okabot 4.10.0')
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## What's new?",
+            "- Scraps! Currently, you can obtain these with daily rewards. However, more ways to earn them will come in the future.",
+            "- Crafting! With these scraps, you can craft various items.",
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## Changes/Bugfixes",
+            "- Updated story viewer to use button components.",
+            "- Fixed a bug where stories could be unlocked without having previous stories unlocked.",
+            "- Updated profile schema to support scraps.",
+            "- Updated /pockets to have a scraps option.",
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('-# thanks for using my bot!')
+    );
