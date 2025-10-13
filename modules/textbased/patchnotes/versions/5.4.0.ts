1a6a4596b18ffe0dca3ee008e8242308426088d8
Millie <oka@waffle.moe>
2025-10-13 02:18:48 -0500

    bump to 5.4.0

---
+++ modules/textbased/patchnotes/versions/5.4.0.ts
@@ -0,0 +1,33 @@
+import {ContainerBuilder, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder} from "discord.js";
+
+
+export const PATCHNOTES_COMPONENT = new ContainerBuilder()
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('# okabot 5.4.0')
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## What's new?",
+            "- okabot 5.4.0 reintroduces stocks. This might go terribly, but honestly who cares.",
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## Changes/Bugfixes",
+            "- Fixed a bug where trying to render a stock would throw an error.",
+            "- Fixed a bug where permissions would return false despite being correct.",
+            "- Brewed Hot Cockolate <3"
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('-# thanks for using my bot!')
+    );
