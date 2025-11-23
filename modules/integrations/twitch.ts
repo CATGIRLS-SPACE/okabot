79950e57e9d97b98ef9597a0bf3d3309497a9d30
Millie <oka@waffle.moe>
2025-11-22 21:14:59 -0600

    throwing and stuff!

--- modules/integrations/twitch.ts
+++ modules/integrations/twitch.ts
@@ -1,8 +1,8 @@
 import {RefreshingAuthProvider} from "@twurple/auth";
 import {BASE_DIRNAME, CONFIG, DEV} from "../../index";
 import {Bot, createBotCommand} from "@twurple/easy-bot";
-import {Message} from "discord.js";
-import {existsSync, mkdirSync, readFileSync, writeFileSync} from "fs";
+import {Message, TextChannel} from "discord.js";
+import {existsSync, readFileSync, writeFileSync} from "fs";
 import {join} from "path";
 
 /*
@@ -10,6 +10,8 @@ import {join} from "path";
     It will likely take a lot of rewriting to get it to working on another channel.
     It contains heavily simplified okabot features for twitch chat.
 */
+
+const TITS_API_URL = 'ws://192.168.1.10:42069/websocket';
 
 interface TwitchDB {
     users: {[key: string]: TwitchProfile}
@@ -21,6 +23,8 @@ interface TwitchProfile {
 }
 
 let LOADED_TWITCH_DB: TwitchDB = {users: {}};
+let TITS_WEBSOCKET: WebSocket;
+let LOG_CHANNEL: TextChannel;
 
 // profile functions?
 
@@ -53,6 +57,7 @@ export async function ActivateTwitchIntegration(message: Message) {
     }
 
     LOADED_TWITCH_DB = JSON.parse(readFileSync(join(BASE_DIRNAME, 'db', 'twitch.oka'), 'utf-8'));
+    LOG_CHANNEL = message.channel as TextChannel;
 }
 
 export async function TwitchSetOauthAndStartBot(token: string) {
@@ -81,12 +86,21 @@ export async function TwitchSetOauthAndStartBot(token: string) {
         obtainmentTimestamp: Math.floor(new Date().getTime() / 1000)
     }, ['chat']);
 
+    try {
+        TITS_WEBSOCKET = new WebSocket(TITS_API_URL);
+        TITS_WEBSOCKET.onopen = () => {
+            LOG_CHANNEL.send('TITS API connected.');
+        }
+    } catch {
+        LOG_CHANNEL.send('TITS API failed to connect');
+    }
+
     new Bot({
         authProvider,
         channel: 'meowlliie',
         commands:[
             createBotCommand('okabot', (params, {reply}) => {
-                reply('okabot now has some limited integration with my Twitch chat. It sort of serves to bridge the gap for channel points while I am not affiliate. Try it out with !okash or !get.');
+                reply('okabot now has some limited integration with my Twitch chat. It sort of serves to bridge the gap for channel points while I am not affiliate. Try it out with !okash, !get, or !throw. More things are coming soon.');
             }),
 
             createBotCommand('okash', (params, {userName, userId, reply}) => {
@@ -104,7 +118,43 @@ export async function TwitchSetOauthAndStartBot(token: string) {
                 profile.okash += rolled_amount;
                 profile.last_get = now;
 
+                LOADED_TWITCH_DB.users[userId] = profile;
+                writeFileSync(join(BASE_DIRNAME, 'db', 'twitch.oka'), JSON.stringify(LOADED_TWITCH_DB));
+
                 reply(`${userName} got ${rolled_amount} added to their okash!`);
+            }),
+
+            createBotCommand('throw', (params, {userName, userId, reply}) => {
+                if (TITS_WEBSOCKET.readyState != WebSocket.OPEN) return reply('Looks like TITS isn\'t working. Sorry');
+
+                const profile = GetTwitchProfile(userId);
+
+                let count = 1;
+                if (params[0] && !isNaN(parseInt(params[0]))) count = Math.max(1, parseInt(params[0]));
+                count = Math.min(count, 10);
+
+                if (profile.okash < count * 125) return reply(`Not enough okash to throw! You need 125 okash per item!`);
+
+                profile.okash -= count * 125;
+                LOADED_TWITCH_DB.users[userId] = profile;
+                writeFileSync(join(BASE_DIRNAME, 'db', 'twitch.oka'), JSON.stringify(LOADED_TWITCH_DB));
+
+                for (let i = 0; i < count; i++)
+                    TITS_WEBSOCKET.send(JSON.stringify(
+                        {
+                            apiName: "TITSPublicAPI",
+                            apiVersion: "1.0",
+                            requestId: `okabot${Math.floor(Math.random() * 1000000)}`,
+                            messageType: "TITSTriggerActivateRequest",
+                            data: {
+                                triggerName: "okabot redeem throw"
+                            }
+                        }
+                    ));
+
+
+                if (count == 1) reply(`${userName} throws an item! Ouch!`);
+                else reply(`${userName} throws ${count} items! Owee!`);
             })
         ]
     }).say('meowlliie', 'okabot is now active and listening for commands in this chat!');
