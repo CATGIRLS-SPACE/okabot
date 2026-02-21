be4945b185e865e64a1a76262664f6250f34396f
Millie <oka@waffle.moe>
2026-02-20 20:17:05 -0600

    bump to 5.5.0

---
+++ modules/textbased/patchnotes/versions/5.5.0.ts
@@ -0,0 +1,34 @@
+import {ContainerBuilder, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder} from "discord.js";
+
+
+export const PATCHNOTES_COMPONENT = new ContainerBuilder()
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('# okabot 5.5.0')
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## What's new?",
+            "- Removed the limitation of one streak restore per streak due to the instability of it and also just annoyance.",
+            "- The ability to delete all account data was added."
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent([
+            "## Changes/Bugfixes",
+            "- Fixed a bug where users could be robbed in servers they were not a part of.",
+            "- Updated rules.",
+            "- Brewed Hot Cockolate <3"
+        ].join('\n'))
+    )
+    .addSeparatorComponents(
+        new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
+    )
+    .addTextDisplayComponents(
+        new TextDisplayBuilder().setContent('-# thanks for using my bot!')
+    );
