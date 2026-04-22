3e4f08192f8f26850c40dba08f87ac6a46fdd1ce
Millie Aurora <okawaffles@gmail.com>
2026-04-21 22:47:57 -0500

    more quake stuff

--- modules/earthquakes/quake-next.ts
+++ modules/earthquakes/quake-next.ts
@@ -1,18 +1,28 @@
 import {Logger} from "okayulogger";
-import {MessageFlags, TextChannel} from "discord.js";
+import {EmbedBuilder, MessageFlags, TextChannel} from "discord.js";
 import {client} from "../../index";
 import {WebSocket} from 'ws';
-import {DefaultComponent} from "lily-dmdata";
+import {EarthquakeInformationSchemaBody, ShindoValue} from "lily-dmdata";
 
 const L = new Logger('earthquakes (next)')
 
 let REPORT_CHANNEL: TextChannel;
+
+export enum ConnectionStatus {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED,
+    AWAITING_RECONNECT,
+    FAILED,
+}
+export let CONNECTION_STATUS = ConnectionStatus.DISCONNECTED;
 
 export async function InitEarthquakes(api_key: string, report_channel: string, is_retry: boolean = false) {
     REPORT_CHANNEL = await client.channels.fetch(report_channel) as TextChannel;
     if (!REPORT_CHANNEL) return L.fatal('Invalid report channel set.');
 
     // get ticket for websocket
+    CONNECTION_STATUS = ConnectionStatus.CONNECTING;
 
     let resp;
 
@@ -32,6 +42,7 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
         });
     } catch (err) {
         console.error(err);
+        CONNECTION_STATUS = ConnectionStatus.FAILED;
         return REPORT_CHANNEL.send({
             content: 'DMData connection failed due to `fetch()` rejection. See logs for info.'
         });
@@ -42,6 +53,7 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
         console.log(data);
 
         if (is_retry) {
+            CONNECTION_STATUS = ConnectionStatus.AWAITING_RECONNECT;
             // try again in 5 minutes
             setTimeout(() => InitEarthquakes(api_key, report_channel, true), 60_000 * 5);
             return REPORT_CHANNEL.send({
@@ -51,6 +63,7 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
         }
 
         L.fatal('Failed to get DMData ticket!');
+        CONNECTION_STATUS = ConnectionStatus.FAILED;
         throw new Error(`DMData ticket retrieval failure: ${JSON.stringify(data)}`);
     }
 
@@ -64,11 +77,13 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
 
     socket.on('open', () => {
         L.info('Socket open success!');
+        CONNECTION_STATUS = ConnectionStatus.CONNECTED;
         REPORT_CHANNEL.send({content:`ᓀ‸ᓂ DMData connection successful!`, flags:[MessageFlags.SuppressNotifications]});
     });
     // error handling
     socket.on('error', (error) => {
         L.warn(`Socket errored: ${error.message}`);
+        CONNECTION_STATUS = ConnectionStatus.AWAITING_RECONNECT;
         REPORT_CHANNEL.send({content:`ᓀ‸ᓂ I was disconnected from DMData because the socket threw an error. I will retry in 60 seconds...`, flags:[MessageFlags.SuppressNotifications]});
         try {
             socket.close();
@@ -81,6 +96,7 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
     });
     socket.on('close', () => {
         L.warn(`Socket closed unexpectedly!`);
+        CONNECTION_STATUS = ConnectionStatus.AWAITING_RECONNECT;
         REPORT_CHANNEL.send({content:`ᓀ‸ᓂ I was disconnected from DMData unexpectedly! I will retry in 60 seconds...`, flags:[MessageFlags.SuppressNotifications]});
         try {
             socket.close();
@@ -113,13 +129,32 @@ export async function InitEarthquakes(api_key: string, report_channel: string, i
 }
 
 
-function ParseData(data: DefaultComponent) {
+function ParseData(data: EarthquakeInformationSchemaBody) {
     if (data.type == 'earthquake-information') {
         // This is an earthquake report
-
+        if (!data.earthquake) return ReportEpicenterUnderInvestigation(data);
     }
 
     if (data.type == 'eew-information') {
         // This is an EEW(F)
     }
+}
+
+
+function ReportEpicenterUnderInvestigation(data: EarthquakeInformationSchemaBody) {
+    REPORT_CHANNEL.send({
+        embeds: [
+            new EmbedBuilder()
+                .setTitle(`A Shindo ${data.intensity.maxInt} earthquake occurred.`)
+                .setColor(0x9d60cc)
+                .addFields({
+                    name: 'Maximum Measured Intensity',
+                    value: data.intensity.maxInt
+                }, {
+                    name: 'Location',
+                    value: 'Under Assessment'
+                })
+        ],
+        flags: data.intensity.maxInt == ShindoValue.ONE || data.intensity.maxInt == ShindoValue.TWO ? [MessageFlags.SuppressNotifications] : []
+    });
 }
